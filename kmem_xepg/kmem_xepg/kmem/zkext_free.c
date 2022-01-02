e08a2a24e4e17696fec9dd030ac05550c2e37f7c
Sreejith Krishnan R <hello.sreejith@pm.me>
2022-01-02 02:49:40 -0800

    Improve logging

--- kmem_xepg/kmem_xepg/kmem/zkext_free.c
+++ kmem_xepg/kmem_xepg/kmem/zkext_free.c
@@ -25,6 +25,10 @@
 #include "util_log.h"
 
 
+#define NUM_SLOW_DOWN_NICS 100
+#define NUM_SOCKETS_PER_SLOW_DOWN_NIC 10000
+
+
 struct kmem_zkext_free_session {
     kmem_zkext_neighour_reader_t reader;
     smb_nic_allocator nic_allocator;
@@ -182,10 +186,14 @@ struct complete_nic_info_entry kmem_zkext_free_session_prepare(kmem_zkext_free_s
 void kmem_zkext_free_session_execute(kmem_zkext_free_session_t session, const struct complete_nic_info_entry* entry) {
     assert(session->state == STATE_PREPARED);
     
-    for (int i = 0; i < 100; i++) {
-        XE_LOG_DEBUG("allocate sockets for nic %d / %d", i, 100);
-        kmem_zkext_free_kext_allocate_sockets(session->nic_allocator, INT32_MAX - i, 10000, sizeof(struct sockaddr_in));
+    printf("[INFO] preparing for free ");
+    for (int i = 0; i < NUM_SLOW_DOWN_NICS; i++) {
+        if ((i % ((NUM_SLOW_DOWN_NICS + 9) / 10)) == 0) {
+            printf(" %d/%d ", i,  NUM_SLOW_DOWN_NICS);
+        }
+        kmem_zkext_free_kext_allocate_sockets(session->nic_allocator, INT32_MAX - i, NUM_SOCKETS_PER_SLOW_DOWN_NIC, sizeof(struct sockaddr_in));
     }
+    printf("done\n");
 
     kmem_zkext_free_kext_allocate_sockets(session->nic_allocator, (uint32_t)entry->nic_index, 512, 96);
     kmem_allocator_nic_parallel_t capture_allocator = kmem_allocator_nic_parallel_create(&session->smb_addr, 1024 * 320);
