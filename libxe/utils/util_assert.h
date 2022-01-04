08cdb0e6ec0efb6e64ee514f3d9329097c513195
Sreejith Krishnan R <hello.sreejith@pm.me>
2022-01-04 07:19:34 -0800

    Assertion to check for valid kernel address

--- libxe/utils/util_assert.h
+++ libxe/utils/util_assert.h
@@ -17,5 +17,6 @@
 #define xe_assert_err(val) if ((val) != 0) { printf("[ERROR] error check failed at %s:%d, err: %s(%d)\n", __FILE__, __LINE__, strerror(val), val); getpass("press enter to exit\n"); abort(); }
 #define xe_assert_errno(res) if ((res) != 0) { printf("[ERROR] error check failed at %s:%d, err: %s(%d)\n", __FILE__, __LINE__, strerror(errno), errno); getpass("press enter to exit\n"); abort(); }
 #define xe_assert_cond(lhs, cond, rhs) if (!((lhs) cond (rhs))) { printf("[ERROR] assert condition %llu %s %llu failed at %s::%d\n", (uint64_t)(lhs), #cond, (uint64_t)(rhs), __FILE__, __LINE__); getpass("press enter to continue\n"); abort(); }
+#define xe_assert_kaddr(val) if (!XE_VM_KERNEL_ADDRESS_VALID((val))) { printf("[ERROR] assertion for valid kernel address failed for %p at %s:%d\n", (void*)val, __FILE__, __LINE__); getpass("press enter to continue"); abort(); }
 
 #endif /* util_assert_h */
