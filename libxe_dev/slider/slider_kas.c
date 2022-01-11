1e17343e2117c7be411acc265596817c8bf87830
Sreejith Krishnan R <hello.sreejith@pm.me>
2022-01-11 07:02:07 -0800

    Dev slider

---
+++ libxe_dev/slider/slider_kas.c
@@ -0,0 +1,33 @@
+//
+//  slider_kas.c
+//  libxe_dev
+//
+//  Created by admin on 1/11/22.
+//
+
+#include <sys/errno.h>
+
+#include "slider_kas.h"
+
+
+/* The slide of the main kernel compared to its static link address */
+#define KAS_INFO_KERNEL_TEXT_SLIDE_SELECTOR         (0)     /* returns uint64_t    */
+#define KAS_INFO_KERNEL_SEGMENT_VMADDR_SELECTOR     (1)
+#define KAS_INFO_MAX_SELECTOR                       (2)
+
+extern int kas_info(int selector, void *value, size_t *size);
+
+
+uintptr_t xe_slider_kas_get_mh_execute_header(void) {
+    size_t num_segments = 0;
+    if (kas_info(KAS_INFO_KERNEL_SEGMENT_VMADDR_SELECTOR, NULL, &num_segments)) {
+        return errno;
+    }
+    
+    uintptr_t kern_segment_bases[num_segments];
+    if (kas_info(KAS_INFO_KERNEL_SEGMENT_VMADDR_SELECTOR, kern_segment_bases, &num_segments)) {
+        return errno;
+    }
+    
+    return kern_segment_bases[0];
+}
---
+++ libxe_dev/slider/slider_kas.h
@@ -0,0 +1,15 @@
+//
+//  slider_kas.h
+//  libxe_dev
+//
+//  Created by admin on 1/11/22.
+//
+
+#ifndef slider_kas_h
+#define slider_kas_h
+
+#include <stdio.h>
+
+uintptr_t xe_slider_kas_get_mh_execute_header(void);
+
+#endif /* slider_kas_h */
