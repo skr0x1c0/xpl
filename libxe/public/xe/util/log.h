431c46e8a98e49d65435865af954895d65daa412
Sreejith Krishnan R <hello.sreejith@pm.me>
2022-03-02 18:45:32 -0800

    Cleanup

--- libxe/public/xe/util/log.h
+++ libxe/public/xe/util/log.h
@@ -10,10 +10,10 @@
 
 #include "binary.h"
 
-#define xe_log_debug(...) printf("[DEBUG] "); printf(__VA_ARGS__); printf("\n")
-#define xe_log_info(...) printf("[INFO] "); printf(__VA_ARGS__); printf("\n")
-#define xe_log_warn(...) printf("[WARN] "); printf(__VA_ARGS__); printf("\n")
-#define xe_log_error(...) printf("[ERROR] "); printf(__VA_ARGS__); printf("\n")
-#define xe_log_debug_hexdump(blob, size, ...) printf("[DEBUG] "); printf(__VA_ARGS__); printf("\n"); xe_util_binary_hex_dump((char*)blob, size)
+#define xe_log_debug(...) printf("[D] %s: ", __FUNCTION__); printf(__VA_ARGS__); printf("\n")
+#define xe_log_info(...) printf("[I] %s: ", __FUNCTION__); printf(__VA_ARGS__); printf("\n")
+#define xe_log_warn(...) printf("[W] %s: ", __FUNCTION__); printf(__VA_ARGS__); printf("\n")
+#define xe_log_error(...) printf("[E] %s: ", __FUNCTION__); printf(__VA_ARGS__); printf("\n")
+#define xe_log_debug_hexdump(blob, size, ...) printf("[D] %s: ", __FUNCTION__); printf(__VA_ARGS__); printf("\n"); xe_util_binary_hex_dump((char*)blob, size)
 
 #endif /* xe_util_log_h */
