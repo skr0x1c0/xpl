7d7232747bf177fa0815e96b051962d76de45630
Sreejith Krishnan R <hello.sreejith@pm.me>
2022-03-25 18:58:50 -0700

    Update build script to handle multiple configurations

--- build.sh
+++ build.sh
@@ -1,19 +1,36 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-mkdir -p build
+CPP_DEFINITIONS="\$GCC_PREPROCESSOR_DEFINITIONS"
+BUILD_DIR="$PWD/build"
 
-xcodebuild -workspace xe.xcworkspace -scheme poc_snb_name_oob_read_server -configuration Release install DSTROOT=$PWD/build
-xcodebuild -workspace xe.xcworkspace -scheme poc_snb_name_oob_read_client -configuration Release install DSTROOT=$PWD/build
-xcodebuild -workspace xe.xcworkspace -scheme poc_sockaddr_oob_read -configuration Release install DSTROOT=$PWD/build
-xcodebuild -workspace xe.xcworkspace -scheme poc_oob_write -configuration Release install DSTROOT=$PWD/build
-xcodebuild -workspace xe.xcworkspace -scheme poc_double_free -configuration Release install DSTROOT=$PWD/build
-xcodebuild -workspace xe.xcworkspace -scheme poc_info_disclosure -configuration Release install DSTROOT=$PWD/build
+case $1 in
+    "MACOS_21E230_T6000_RELEASE")
+        CPP_DEFINITIONS="$CPP_DEFINITIONS MACOS_21E230_T6000_RELEASE=1"
+        BUILD_DIR="$BUILD_DIR/T6000"
+        ;;
+    "MACOS_21E230_T8101_RELEASE")
+        CPP_DEFINITIONS="$CPP_DEFINITIONS MACOS_21E230_T8101_RELEASE=1"
+        BUILD_DIR="$BUILD_DIR/T8101"
+        ;;
+    *)
+        echo "[ERROR] invalid build configuration $1"
+        echo "usage: build.sh <build configuration>"
+        echo "valid configurations are MACOS_21E230_T6000_RELEASE and MACOS_21E230_T8101_RELEASE"
+        exit 1
+        ;;
+esac
 
-xcodebuild -workspace xe.xcworkspace -scheme xe_smbx -configuration Release install DSTROOT=$PWD/build
-xcodebuild -workspace xe.xcworkspace -scheme xe_kmem -configuration Release install DSTROOT=$PWD/build
-xcodebuild -workspace xe.xcworkspace -scheme demo_sb -configuration Release install DSTROOT=$PWD/build
-xcodebuild -workspace xe.xcworkspace -scheme demo_entitlements -configuration Release install DSTROOT=$PWD/build
-xcodebuild -workspace xe.xcworkspace -scheme demo_wp_disable -configuration Release install DSTROOT=$PWD/build
-xcodebuild -workspace xe.xcworkspace -scheme demo_av -configuration Release install DSTROOT=$PWD/build
-xcodebuild -workspace xe.xcworkspace -scheme demo_sudo -configuration Release install DSTROOT=$PWD/build
-xcodebuild -workspace xe.xcworkspace -scheme test_general_remote -configuration Release install DSTROOT=$PWD/build
+mkdir -p $BUILD_DIR
+
+# POCs
+SCHEMES=(poc_snb_name_oob_read_server poc_snb_name_oob_read_client poc_sockaddr_oob_read poc_oob_write poc_double_free poc_info_disclosure)
+# Demos
+SCHEMES+=(demo_sb demo_entitlements demo_wp_disable demo_av demo_sudo)
+# SMB
+SCHEMES+=(xe_smbx xe_kmem)
+# Test
+SCHEMES+=(test_general_remote)
+
+for SCHEME in ${SCHEMES[@]}; do
+    xcodebuild -workspace xe.xcworkspace -scheme $SCHEME -configuration Release install DSTROOT=$BUILD_DIR GCC_PREPROCESSOR_DEFINITIONS="$CPP_DEFINITIONS" || exit 1
+done
